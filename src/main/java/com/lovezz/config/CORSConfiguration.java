5f6c63443694834cb324610a2271051da68f6c1b
LiWeijian <352642663@qq.com>
2020-01-15 22:30:41 +0800

    新增分支

---
+++ src/main/java/com/lovezz/config/CORSConfiguration.java
@@ -0,0 +1,17 @@
+package com.lovezz.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+//@Configuration
+public class CORSConfiguration extends WebMvcConfigurationSupport {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowedHeaders("*");
+        super.addCorsMappings(registry);
+    }
+}
--- src/main/java/com/lovezz/controller/MenuController.java
+++ src/main/java/com/lovezz/controller/MenuController.java
@@ -1,11 +1,15 @@
 package com.lovezz.controller;
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.lovezz.dto.BaseResult;
 import com.lovezz.entity.TbMenu;
 import com.lovezz.service.TbMenuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -17,6 +21,7 @@ import java.util.List;
  */
 @Controller
 @RequestMapping("/menu")
+@CrossOrigin("*")
 public class MenuController {
 
     @Autowired
@@ -35,5 +40,12 @@ public class MenuController {
     public ModelAndView index(ModelAndView modelAndView){
         modelAndView.setViewName("index");
         return modelAndView;
+    }
+
+    @GetMapping("/getMenuList")
+    @ResponseBody
+    public BaseResult getMenuList(){
+        List<TbMenu> menuList = menuService.selectList(new EntityWrapper<TbMenu>().eq("isDelete", "1"));
+        return BaseResult.success("成功",menuList);
     }
 }
